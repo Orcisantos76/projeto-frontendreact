d0509302b4a654ef30a4532510bb05e43665ed3e
Orcisantos76 <orcisantos76@gmail.com>
2023-05-27 21:59:28 -0300

    alterei a ordenacao

--- projeto-frontendreact/src/components/ProductList/Home/index.jsx
+++ projeto-frontendreact/src/components/ProductList/Home/index.jsx
@@ -1,52 +1,67 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ProductCard from '../ProductCard'
-import { Topo,Cards,Container } from './style'
+import { Topo, Cards, Container } from './style'
 
-function Home({productList, 
-    amount, 
+function Home({ productList,
+    amount,
     setAmount,
     cart,
     setCart,
     minFilter,
-    MaxFilter,
-    searchFilter}){
+    maxFilter,
+    searchFilter }) {
 
-    const [ordination, setOrdination]= useState('')
+    const [ordination, setOrdination] = useState('')
+    const [productListFiltered, setProductListFiltered] = useState(...productList)
 
-    const onChangeOrdination = (event)=>{        
+    const onChangeOrdination = (event) => {
         setOrdination(event.target.value)
     }
     // ----------------------------------------------
-    function addToCart(product){        
+    function addToCart(product) {
         const newProduct = cart.find(
-            (produtoCallback)=> product.id === produtoCallback.id);
+            (produtoCallback) => product.id === produtoCallback.id);
 
-            if (newProduct === undefined){
-                product ={...product,quantidade: 1};
-                setCart([...cart, product]);
-                const valorTotal = amount + product.value
-                // setCart é a mudança, vai receber o array cart, e adicionar product a ele, usando spreed operator
-                setAmount(valorTotal) 
-                
-            }else {
-                const novoCarrrinho = cart.map((product)=>{
-                    if (product.id === newProduct.id){
-                        const valorTotal  = amount + product.value;
-                        setAmount(valorTotal)
-                        return {...newProduct,quantidade: product.quantidade + 1};
-                    } else {
-                        return product;
-                    }
-                });
-                setCart(novoCarrrinho);
-                
+        if (newProduct === undefined) {
+            product = { ...product, quantidade: 1 };
+            setCart([...cart, product]);
+            const valorTotal = amount + product.value
+            // setCart é a mudança, vai receber o array cart, e adicionar product a ele, usando spreed operator
+            setAmount(valorTotal)
+
+        } else {
+            const novoCarrrinho = cart.map((product) => {
+                if (product.id === newProduct.id) {
+                    const valorTotal = amount + product.value;
+                    setAmount(valorTotal)
+                    return { ...newProduct, quantidade: product.quantidade + 1 };
+                } else {
+                    return product;
+                }
+            });
+            setCart(novoCarrrinho);
+
+        }
+    } console.log(productList)
+
+    useEffect(() => {
+        const productListFiltered = [...productList].sort((min, max) => {
+            if (ordination === "Crescente") {
+                return min.value - max.value;
+            } else if (ordination === "Decrescente") {
+                return max.value - min.value;
+            } else {
+                return 0;
             }
-    }console.log("Valor total",amount)
+        });
+
+        setProductListFiltered(productListFiltered)
+    }, [productList, minFilter, maxFilter, searchFilter, ordination])
 
     // -----------------------------------------------
-    return (  
+    return (
 
         <Container>
 
@@ -54,30 +69,30 @@ function Home({productList,
                 <div>
                     <p>Quantidade de produtos: {productList.length}</p>
                 </div>
-                
+
                 <div className='ordenacao'>
                     <p>Ordenação</p>
                     <form type="select">
                         <select value={ordination} onChange={onChangeOrdination}>
-                            <option>Selecione</option>
-                            <option>Crescente</option>
-                            <option>Decrescente</option>
+                            <option value="">Selecione</option>
+                            <option value="Crescente">Crescente</option>
+                            <option value="Decrescente">Decrescente</option>
                         </select>
                     </form>
                 </div>
             </Topo>
             <Cards>
-                {productList.map((product)=>{
-                return <ProductCard 
-                        key={product.id} 
-                        product={product} 
-                        adicionarAoCarrinho={addToCart}/>
+                {productListFiltered.map((product) => {
+                    return <ProductCard
+                        key={product.id}
+                        product={product}
+                        adicionarAoCarrinho={addToCart} />
                 })
-                // chamada do componente utilizando o map
+                    // chamada do componente utilizando o map
                 }
 
             </Cards>
-            
+
         </Container>
     )
 }
